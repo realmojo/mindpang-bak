9787902efa82bb8b135ede72a33857201a572048
realmojo <strikers1999@naver.com>
2023-02-18 18:14:12 +0900

    update

---
+++ sitemap-common.js
@@ -0,0 +1,75 @@
+const axios = require("axios");
+const fs = require("fs");
+const prettier = require("prettier");
+
+// 오늘 날짜 가져오기 & 도메인 설정
+const DOMAIN = "https://mindpang.com";
+const formatted = (sitemap) => prettier.format(sitemap, { parser: "html" });
+
+const replaceAll = (str, searchStr, replaceStr) => {
+  return str.split(searchStr).join(replaceStr);
+};
+
+(async () => {
+  const { data } = await axios.get(
+    "https://f5game.co.kr/api/test/getTests.php"
+  );
+  const pagesSitemap = `${data.map((item) => {
+    return `<url>
+            <loc>${DOMAIN}/${item.category}/${encodeURI(item.link)}</loc>
+            <lastmod>${item.regdate.substring(0, 10)}</lastmod>
+          </url>
+          <url>
+            <loc>${DOMAIN}/${item.category}/${encodeURI(item.link)}/play</loc>
+            <lastmod>${item.regdate.substring(0, 10)}</lastmod>
+          </url>
+          `;
+  })}`;
+
+  const generatedSitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset
+      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+     xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+      <url>
+        <loc>${DOMAIN}</loc>
+        <lastmod>2023-01-01</lastmod>
+        <changefreq>always</changefreq>
+        <priority>1.00</priority>
+      </url>
+      <url>
+        <loc>${DOMAIN}/life</loc>
+        <lastmod>2023-01-01</lastmod>
+        <changefreq>always</changefreq>
+        <priority>1.00</priority>
+      </url>
+      <url>
+        <loc>${DOMAIN}/love</loc>
+        <lastmod>2023-01-01</lastmod>
+        <changefreq>always</changefreq>
+        <priority>1.00</priority>
+      </url>
+      <url>
+        <loc>${DOMAIN}/health</loc>
+        <lastmod>2023-01-01</lastmod>
+        <changefreq>always</changefreq>
+        <priority>1.00</priority>
+      </url>
+      <url>
+        <loc>${DOMAIN}/money</loc>
+        <lastmod>2023-01-01</lastmod>
+        <changefreq>always</changefreq>
+        <priority>1.00</priority>
+      </url>
+      <url>
+        <loc>${DOMAIN}/entertain</loc>
+        <lastmod>2023-01-01</lastmod>
+        <changefreq>always</changefreq>
+        <priority>1.00</priority>
+      </url>
+      ${replaceAll(pagesSitemap, ",", "")}
+    </urlset>`;
+
+  const formattedSitemap = [formatted(generatedSitemap)];
+  fs.writeFileSync("./public/sitemap.xml", formattedSitemap[0], "utf8");
+})();
