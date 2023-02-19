79bd2bbea21a1969e4a1c55af2fdddc9c7c9a0e6
realmojo <strikers1999@naver.com>
2023-02-19 18:01:30 +0900

    window remove

--- plugins/naver-analytics.js
+++ plugins/naver-analytics.js
@@ -1,7 +1,7 @@
 export default defineNuxtPlugin(() => {
-  if (!wcs_add) var wcs_add = {};
-  wcs_add["wa"] = "18cf866f0226840";
-  if (window.wcs) {
-    wcs_do();
-  }
+  // if (!wcs_add) var wcs_add = {};
+  // wcs_add["wa"] = "18cf866f0226840";
+  // if (window.wcs) {
+  //   wcs_do();
+  // }
 });
